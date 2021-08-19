5dfc89e2e24e6492a5ee75396fdc7f775cedbeb5
subashnalabothu <jyothisaisubhashnalabothuj@gmail.com>
2021-08-20 04:26:44 +0530

    added introduction screen

--- app/src/main/java/uk/ac/tees/aad/W9462875/IntroductionScreen.java
+++ app/src/main/java/uk/ac/tees/aad/W9462875/IntroductionScreen.java
@@ -1,18 +1,17 @@
 package uk.ac.tees.aad.W9462875;
 
-import  androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 
 
-
-public class MainActivity extends AppCompatActivity {
+public class IntroductionScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_introduction_screen);
 
     }
 }

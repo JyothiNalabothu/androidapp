15197fd9d88f5563a6ae41f313f18aff281412f2
subashnalabothu <jyothisaisubhashnalabothuj@gmail.com>
2021-08-20 09:53:52 +0530

    added location and map

--- app/src/main/java/uk/ac/tees/aad/W9462875/CharityView.java
+++ app/src/main/java/uk/ac/tees/aad/W9462875/CharityView.java
@@ -3,9 +3,12 @@ package uk.ac.tees.aad.W9462875;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,17 +16,30 @@ import com.bumptech.glide.Glide;
 
 public class CharityView extends AppCompatActivity {
 
+    SharedPreferences sharedPref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_charity_view);
+         sharedPref = getSharedPreferences("selectedCharity", Context.MODE_PRIVATE);
 
         ImageView imageView = findViewById(R.id.imagemain);
         TextView title  = findViewById(R.id.textViewtitle);
         TextView description = findViewById(R.id.textViewdes);
         TextView bank   =  findViewById(R.id.textViewbank);
+        Button btn = findViewById(R.id.viewin);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent =  new Intent(getApplicationContext(),MapsActivity.class);
+               intent.putExtra("lat",sharedPref.getFloat("lat",0));
+               intent.putExtra("lng",sharedPref.getFloat("lng",0));
+               intent.putExtra("name",sharedPref.getString("name",""));
+               startActivity(intent);
+            }
+        });
 
-        SharedPreferences sharedPref = getSharedPreferences("selectedCharity", Context.MODE_PRIVATE);
+
 
         title.setText(sharedPref.getString("name",""));
         description.setText(sharedPref.getString("des",""));
--- app/src/main/java/uk/ac/tees/aad/W9462875/MapsActivity.java
+++ app/src/main/java/uk/ac/tees/aad/W9462875/MapsActivity.java
@@ -26,22 +26,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        String title = getIntent().getStringExtra("name");
+        LatLng location = new LatLng(getIntent().getFloatExtra("lat",0),getIntent().getFloatExtra("lng",0));
+        mMap.addMarker(new MarkerOptions().position(location).title(title)).showInfoWindow();
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 12.0f));
+
+
     }
 }
